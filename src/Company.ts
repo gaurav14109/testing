7669def30985ab6b60c893a7dad245c90684fc4a
Gaurav <gauravgusain48@gmail.com>
2022-04-26 10:49:55 +0530

    done

--- src/Company.ts
+++ src/Company.ts
@@ -9,7 +9,7 @@ export class Company implements Mappable{ //optional
   }
   color:string='black'
   constructor() {
-    console.log('Hi')
+    console.log('Hii')
 
     this.companyName = faker.company.companyName();
     this.catchPhrase = faker.company.catchPhrase();
